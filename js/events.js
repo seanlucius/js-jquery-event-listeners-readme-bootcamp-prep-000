fdb2b35dadbb7d8357789f8432d78ce25cdadd3d
Sean Lucius <seanlucius22@icloud.com>
2018-09-01 04:48:33 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,22 @@
 //define functions here
+function getIt() {
+  
+  $('p').on('click', function() {
+    
+    alert('Hey!');
+    
+  });
+}
+
+function frameIt() {
+  
+  $('img').on('load', function() {
+    
+    
+    
+  })
+  
+}
 
 $(document).ready(function(){
 
